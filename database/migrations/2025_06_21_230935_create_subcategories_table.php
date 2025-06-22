4b1517d7ed93012de9eac2079f51a44776128efd
Jesus <csc.jesus@gmail.com>
2025-06-22 12:55:20 -0600

    Llenando families, categories y subcategories con datos de prueba

--- database/migrations/2025_06_21_230935_create_subcategories_table.php
+++ database/migrations/2025_06_21_230935_create_subcategories_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('subcategories', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
+            $table->string('name');
             $table->foreignId('category_id')
                 ->constrained();
             $table->timestamps();
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,13 +11,16 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
+    //php artisan db:seed
+    //php artisan migrate:refresh --seed
     public function run(): void
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+        $this->call([FamilySeeder::class]);
     }
 }
