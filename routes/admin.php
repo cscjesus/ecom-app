bf8b2c602bbc7eb4691e75d9c27135c0018d5e9f
Jesus <csc.jesus@gmail.com>
2025-06-23 09:03:24 -0600

    Dashboar de admin terminado

--- routes/admin.php
+++ routes/admin.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 //hay que registrar el archivo de rutas en el archivo bootstrap/app.php
 Route::get('/', function () {
-    return "Hola desde el administrador";
+    return view('admin.dashboard');
 })->name('dashboard');
 
